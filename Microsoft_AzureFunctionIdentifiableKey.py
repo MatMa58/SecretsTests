e6632f05ac2c3939ff568381f42ab3ab43dc2fd5
MatMa58 <mateusz.majstrak@ing.com>
2025-01-09 09:32:44 +0100

    Add files via upload

---
+++ Microsoft_AzureFunctionIdentifiableKey.py
@@ -0,0 +1,26 @@
+import requests
+
+# This is a test key to check secret scanning detection
+event_grid_endpoint = "https://your-event-grid-endpoint.westus2-1.eventgrid.azure.net/api/events"
+event_grid_key = "_BpRFgL3Bw5GI1FsQp307znSi71qz2aTl0l6Z3vx6DvdAzFuvuAO7Q=="
+# this key is not real :)
+
+headers = {
+    "Content-Type": "application/json",
+    "aeg-sas-key": event_grid_key,
+}
+
+# Sending a sample event
+event = [
+    {
+        "id": "1",
+        "eventType": "My.Custom.Event",
+        "subject": "/example/subject",
+        "eventTime": "2023-01-01T00:00:00Z",
+        "data": {"key": "value"},
+        "dataVersion": "1.0",
+    }
+]
+
+response = requests.post(event_grid_endpoint, json=event, headers=headers)
+print(response.status_code)
